84de10f2dcd1c733315647c3397e47724e527138
Tien Thinh <mail@tienthinh.com>
2018-05-20 04:33:18 +0200

    Add SASS/SCSS/CSS webpack compiler

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './components/App'
+import './sass/app.sass'
+
+ReactDom.render(<App/>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // The regular expression to take all .js* files for Babel to compile
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    })
+
+  ]
+}
