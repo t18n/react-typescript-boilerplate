a036fd891321dcb871f6f07d98e74f8bf2fa9878
Tien Thinh <mail@tienthinh.com>
2018-06-03 03:42:31 +0200

    Added Bootstrap 4

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import icon from '../assets/images/webpack.png'
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>React App boilerplate</h1>
         <img src={icon} />
         <small> From Turbo </small>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import App from './components/App'
 import './sass/app.sass'
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDom.render(<App/>, document.getElementById('root'));
