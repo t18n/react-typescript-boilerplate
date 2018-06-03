3dcf179087d41ed0d5fdff92ce5665d01a16f122
Tien Thinh <mail@tienthinh.com>
2018-06-03 17:29:55 +0200

    Add BootstrapJS + JQuery + Popper

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -6,6 +6,31 @@ class App extends Component {
   render() {
     return (
       <div className="container">
+
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a className="navbar-brand" href="#">Hidden brand</a>
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+      
         <h1>React App boilerplate</h1>
         <img src={icon} />
         <small> From Turbo </small>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './components/App/app'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 import './sass/app.sass'
-import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDom.render(<App/>, document.getElementById('root'));
